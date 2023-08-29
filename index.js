56d05e9390e207447ab7d3c73b984b7d07311424
Anshumaan Baaghi <ganumumbai067@gmail.com>
2023-08-29 16:15:53 +0530

    deployed_jsonServer_Added

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5500",
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST","PUT"],
   },
 });
 
