16d982cc5634c18b171357205e6cef6ad0b43200
Anshumaan Baaghi <ganumumbai067@gmail.com>
2023-08-29 15:39:18 +0530

    deployed_jsonServer_Added

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ io.on("connection", (socket) => {
 });
 */
 
-const { default: axios } = require('axios');
-const dotenv = require('dotenv')
-dotenv.config()
+const { default: axios } = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const express = require("express");
 const app = express();
@@ -28,38 +28,46 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.send("Working")
+  res.send("Working");
 });
-let obj = {};
-const fetchdata=async()=>{
-  const res = await axios('https://websocket-trial-jsonserver.onrender.com/Users/1');
-  obj = res.data
-  console.log('res:', res.data)
-}
+const fetchdata = async () => {
+  const res = await axios(
+    "https://websocket-trial-jsonserver.onrender.com/Users/1"
+  );
+  return res.data;
+};
 fetchdata();
-io.on("connection", (socket) => {
-  socket.on("new-user-joined", async(name) => {
-    console.log("name:", name);
-    await fetchdata();
-    obj[socket.id] = name;
-    await axios.put('https://websocket-trial-jsonserver.onrender.com/Users/1',obj)
-    socket.broadcast.emit("user-joined", name);
+setTimeout(() => {
+  io.on("connection", (socket) => {
+    socket.on("new-user-joined", async (name) => {
+      console.log("name:", name);
+      const obj = await fetchdata();
+      obj[socket.id] = name;
+      await axios.put(
+        "https://websocket-trial-jsonserver.onrender.com/Users/1",
+        obj
+      );
+      socket.broadcast.emit("user-joined", name);
+    });
+    socket.on("messageSent", (message) => {
+      socket.broadcast.emit("receive", { message, name: obj[socket.id] });
+    });
+    socket.on("disconnect", async () => {
+      socket.broadcast.emit("left", obj[socket.id]);
+      const obj = await fetchdata();
+      delete obj[socket.id];
+      await axios.put(
+        "https://websocket-trial-jsonserver.onrender.com/Users/1",
+        obj
+      );
+    });
   });
-  socket.on("messageSent", (message) => {
-    socket.broadcast.emit("receive", { message, name: obj[socket.id] });
-  });
-  socket.on("disconnect", async() => {
-    socket.broadcast.emit("left", obj[socket.id]);
-    await fetchdata(); 
-    delete obj[socket.id];
-    await axios.put('https://websocket-trial-jsonserver.onrender.com/Users/1',obj)
-  });
-});
+}, 1000);
 
 const port = process.env.PORT || 3000;
-console.log('process.env.PORT:', process.env.PORT)
+console.log("process.env.PORT:", process.env.PORT);
 
 server.listen(port, () => {
   console.log(`listening on *:${port}`);
